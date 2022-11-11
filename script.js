00d358546159582bd67f3d015db6b247de228b8d
Claudio-DiGiovanni <digioclaudio97@gmail.com>
2022-11-11 15:14:54 +0100

    consegna

---
+++ script.js
@@ -0,0 +1,7 @@
+new Vue ({
+    el: "#root",
+    data: {
+        message: "Hello World",
+        imgSrc : "https://th.bing.com/th/id/R.a66717a6acd4f2aa688de320627b1e4e?rik=BxyjR8E%2fs7tWCg&pid=ImgRaw&r=0"
+    }
+})
